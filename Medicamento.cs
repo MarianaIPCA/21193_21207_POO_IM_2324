d2386f506ecf4325aec4009110fda39a2d220735
rocha <rocha@MARI>
2023-11-15 00:11:03 +0000

    Add project files.

---
+++ Medicamento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum TipoMedi
+{
+    Analgesico,
+    Antibiotico
+}
+namespace _21193_21207
+{
+    public class Medicamento
+    {
+        #region Atributos
+
+        private int nome;
+        private TipoMedi tipo;
+        DateTime dataValidade;
+
+        #endregion
+
+        #region Metodos
+
+        #region Construtores
+
+
+        #endregion
+
+        #endregion
+    }
+}
